4186bddcfababbefefcec8e45b02606637af5253
Fatih-Eraslan <154984350+Fatih-Eraslan@users.noreply.github.com>
2024-02-19 18:14:49 +0300

    Add files via upload

---
+++ 01. tekrar-1.py.py
@@ -0,0 +1,17 @@
+
+def enter_note():
+ not1=int(input("1.sinav notunu giriniz= "))
+ not2=int(input("2.sinav notunu giriniz= "))
+ performans=int(input("performans notunyu giriniz= "))
+ ortalama=not1+not2+performans/3
+ return (ortalama)
+
+def note_control(ortalama):
+ if (ortalama>=50):
+    print("dersten geçtiniz.")
+ else:
+    print("dersten kaldiniz.")
+
+ortalama = enter_note()
+note_control(ortalama)
+
